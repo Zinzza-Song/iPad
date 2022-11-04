2a2774151c45d7bb0c5f0163a715e5e3957702a6
Zinzza-Song <thdgksq@naver.com>
2022-11-04 23:23:09 +0900

    프로젝트 시작

---
+++ README.md
@@ -0,0 +1,18 @@
+# JS로 각 프레임 위치 계산하기
+```js
+let x = 0
+let y = 0
+let frames = ''
+for (let i = 0; i < 60; i += 1) {
+  // frames += `${(100 / 60 * i).toFixed(2)}% { background-position: ${x}px ${y}px; }<br />` // HTML으로 출력!
+  frames += `${(100 / 60 * i).toFixed(2)}% { background-position: ${x}${x === 0 ? '' : 'px'} ${y}${y === 0 ? '' : 'px'}; }\n`
+  if (x <= -500) {
+    x = 0
+    y -= 100
+    continue // 현재 반복을 종료하고 다음 반복으로 넘어가기!
+  }
+  x -= 100
+}
+// document.body.innerHTML = frames // HTML으로 출력!
+console.log(frames)
+```
---
+++ js/main.js
@@ -0,0 +1,170 @@
+import ipads from '../data/ipads.js'
+import navigations from '../data/navigations.js';
+import navigions from '../data/navigations.js'
+
+// 장바구니
+const basketStarterEl = document.querySelector('header .basket-starter');
+const basketEl = basketStarterEl.querySelector('.basket');
+
+basketStarterEl.addEventListener('click', function (event) {
+
+  event.stopPropagation(); // 이벤트 버블링 억제
+
+  if (basketEl.classList.contains('show')) {
+    //hide
+    hideBasket();
+  } else {
+    // show
+    showBasket();
+  }
+});
+basketEl.addEventListener('click', function (event) {
+  event.stopPropagation(); // 이벤트 버블링 억제
+});
+
+window.addEventListener('click', hideBasket);
+
+function showBasket() {
+  basketEl.classList.add('show');
+}
+function hideBasket() {
+  basketEl.classList.remove('show');
+}
+
+
+// 검색
+const headerEl = document.querySelector('header');
+const headerMenuEls = [...headerEl.querySelectorAll('ul.menu > li')]; // 얕은 복사
+const searchWrapEl = headerEl.querySelector('.search-wrap');
+const searchStarterEl = headerEl.querySelector('.search-starter');
+const searchCloserEl = searchWrapEl.querySelector('.search-closer');
+const searchShadowEl = searchWrapEl.querySelector('.shadow');
+const searchInputEl = searchWrapEl.querySelector('input');
+const searchDelayEls = [...searchWrapEl.querySelectorAll('li')];
+
+searchStarterEl.addEventListener('click', showSearch);
+searchCloserEl.addEventListener('click', hideSearch);
+searchShadowEl.addEventListener('click', hideSearch);
+
+function showSearch() {
+  headerEl.classList.add('searching');
+  document.documentElement.classList.add('fixed');
+  headerMenuEls.reverse().forEach(function (el, index) {
+    el.style.transitionDelay = index * .4 / headerMenuEls.length + 's';
+  });
+  searchDelayEls.forEach(function (el, index) {
+    el.style.transitionDelay = index * .4 / searchDelayEls.length + 's';
+  });
+  setTimeout(function () {
+    searchInputEl.focus();
+  }, 600);
+}
+
+function hideSearch() {
+  headerEl.classList.remove('searching');
+  document.documentElement.classList.remove('fixed');
+  headerMenuEls.reverse().forEach(function (el, index) {
+    el.style.transitionDelay = index * .4 / headerMenuEls.length + 's';
+  });
+  searchDelayEls.reverse().forEach(function (el, index) {
+    el.style.transitionDelay = index * .4 / searchDelayEls.length + 's';
+  });
+  searchDelayEls.reverse();
+  searchInputEl.value = '';
+}
+
+
+// 요소의 가시성 관찰
+const io = new IntersectionObserver(function (entries) {
+  entries.forEach(function (entry) {
+    if (!entry.isIntersecting) {
+      return;
+    }
+    entry.target.classList.add('show');
+  });
+});
+
+const infoEls = document.querySelectorAll('.info');
+infoEls.forEach(function (el) {
+  io.observe(el);
+});
+
+
+// 비디오 제어
+const video = document.querySelector('.stage video');
+const playBtn = document.querySelector('.stage .controller--play');
+const pauseBtn = document.querySelector('.stage .controller--pause');
+
+playBtn.addEventListener('click', function () {
+  video.play();
+  playBtn.classList.add('hide');
+  pauseBtn.classList.remove('hide');
+});
+pauseBtn.addEventListener('click', function () {
+  video.pause();
+  playBtn.classList.remove('hide');
+  pauseBtn.classList.add('hide');
+});
+
+
+// '당신에게 맞는 iPad는?' 렌더링
+const itemsEl = document.querySelector('section.compare .items');
+ipads.forEach(function (ipad) {
+  const itemEl = document.createElement('div');
+  itemEl.classList.add('item');
+
+  let colorList = '';
+  ipad.colors.forEach(function (color) {
+    colorList += /* html */ `
+    <li style="background-color: ${color}"></li>
+    `;
+  });
+
+  itemEl.innerHTML = /* html */ `
+  <div class="thumbnail">
+    <img src="${ipad.thumbnail}" alt="${ipad.name}">
+  </div>
+  <ul class="colors">
+    ${colorList}
+  </ul>
+  <h3 class="name">${ipad.name}</h3>
+  <p class="tagline">${ipad.tagline}</p>
+  <p class="price">₩${(ipad.price).toLocaleString('en-us')}부터</p>
+  <button class="btn">구입하기</button>
+  <a href="${ipad.url}" class="link">더 알아보기</a>
+  `;
+
+  itemsEl.append(itemEl);
+});
+
+
+// 데이터 기반 네비게이션 표시
+const navigationsEl = document.querySelector('footer .navigations');
+navigations.forEach(function (nav) {
+  const mapEl = document.createElement('div');
+  mapEl.classList.add('map');
+
+  let mapList = '';
+  nav.maps.forEach(function (map) {
+    mapList += /* html */ `
+    <li>
+      <a href="${map.url}">${map.name}</a>
+    </li>
+    `;
+  });
+
+  mapEl.innerHTML = /* html */ `
+  <h3>
+    <span class="text">${nav.title}</span>
+  </h3>
+  <ul>
+    ${mapList}
+  </ul>
+  `;
+
+  navigationsEl.append(mapEl);
+});
+
+// 연도 표시
+const thisYearEl = document.querySelector('span.this-year');
+thisYearEl.textContent = new Date().getFullYear();
